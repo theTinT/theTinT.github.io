ecec67518f36851c01c6c4109b3781258160b33e
IrynaShv <iryna.shvydchenko@gmail.com>
2015-11-28 17:36:52 -0500

    Added check

--- js/brain.js
+++ js/brain.js
@@ -18,14 +18,73 @@ myApp.controller('SuDuoKuController', function ($scope, $firebase) {
                 }
                 return (currentValue || 0) +1;
             }, function(error,comitted,snapshot) {
-
             });
         }
-
     };
 
     $scope.Restart = function() {
         dataRef.set(initialBoard);
+    };
+
+    $scope.Submit = function() {
+      var correct = $scope.Check();
+        if (correct){
+            alert('CONGRATS YOU SEXY MOFO');
+        }else{
+            alert('BOO HOO TRY AGAIN');
+        }
+    };
+
+    $scope.Check = function() {
+        var colSum = 0;
+        var rowSum = 0;
+        var squareArr = [0,0,0,0,0,0,0,0,0];
+        for (var row = 0; row < 9; row++){
+            for(var col = 0; col < 9; col++){
+                rowSum += $scope.board[row].column[col].value;
+                colSum += $scope.board[col].column[row].value;
+            }
+            if (colSum != 45 ||rowSum != 45){
+                return false;
+            }else{
+                colSum = 0;
+                rowSum = 0;
+            }
+            for(var k = 0; k < 9; k++){
+                if(row < 3) {
+                    if (k < 3) {
+                        squareArr[0] += $scope.board[row].column[k].value;
+                    }else if (k < 6) {
+                        squareArr[1] += $scope.board[row].column[k].value;
+                    }else{
+                        squareArr[2] += $scope.board[row].column[k].value;
+                    }
+                }else if(row < 6) {
+                   if (k < 3) {
+                       squareArr[3] += $scope.board[row].column[k].value;
+                   }else if(k < 6) {
+                       squareArr[4] += $scope.board[row].column[k].value;
+                   }else{
+                       squareArr[5] += $scope.board[row].column[k].value;
+                    }
+                }else{
+                    if (k < 3) {
+                        squareArr[6] += $scope.board[row].column[k].value;
+                    }else if(k < 6) {
+                        squareArr[7] += $scope.board[row].column[k].value;
+                    }else{
+                        squareArr[8] += $scope.board[row].column[k].value;
+                    }
+                }
+            }
+        }
+        for(var i = 0; i < 9; i++) {
+            if (squareArr[i] != 45) {
+                console.log(squareArr[i]);
+                return false;
+            }
+        }
+        return true;
     };
 
     dataRef.on('value', function(snapshot) {
@@ -40,268 +99,7 @@ myApp.controller('SuDuoKuController', function ($scope, $firebase) {
         return rowNumber % 3 === 0;
     };
     
-    $scope.board = [{
-        column: [{
-            value: 5,
-            locked: true
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: 3,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }, {
-        column: [{
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: false
-        }, {
-            value: null,
-            locked: true
-        }]
-    }];
+    $scope.board = [];
 
     var initialBoard = [{
         column: [{value: 5, locked: true},{value:null, locked: false},{value:null, locked: false},{value:null, locked: false},{value:8, locked: true},{value:null, locked: false},{value:2, locked: true},{value:null, locked: false},{value:9, locked: true}]
